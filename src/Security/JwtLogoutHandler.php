5f79cec004c69d949fcecdadf8482a04d61d7547
AbdallahOuahib <ouahibabdallah@gmail.com>
2020-05-06 19:04:41 +0000

    JWT Auth

---
+++ src/Security/JwtLogoutHandler.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+
+class JwtLogoutHandler implements LogoutSuccessHandlerInterface
+{
+    //search this AuthenticationFailureHandlerInterface
+    public function onLogoutSuccess(Request $request)
+    {
+        $response = new JsonResponse(['result' => true]);
+        $response->headers->clearCookie("jwt");
+        return $response;
+    }
+}
