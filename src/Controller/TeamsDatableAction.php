f5901e35a337394f216ba87b258dad05ad15397c
AbdallahOuahib <ouahibabdallah@gmail.com>
2020-06-03 01:10:53 +0100

    Fix API Team DataTable

--- src/Controller/TeamsDatableAction.php
+++ src/Controller/TeamsDatableAction.php
@@ -23,28 +23,8 @@ class TeamsDatableAction
 
     public function __invoke()
     {
-        $arrayReturn = [];
         $teamRepository = $this->entityManager->getRepository(Team::class);
         $teams = $teamRepository->findAllDataTable();
-
-        dd($teams);
-
-        foreach( $users as $key => $user ){
-            $arrayReturn[$key]["idMember"] = $user["idMember"];
-            $arrayReturn[$key]["username"] = $user["username"];
-            $arrayReturn[$key]["email"] = $user["email"];
-            if( in_array("ROLE_DEV",$user["roles"]) ){
-                $arrayReturn[$key]["roles"] = "Développeur";
-            } else {
-                $arrayReturn[$key]["roles"] = "Membre";
-            }
-            $arrayReturn[$key]["userenabled"] = $user["userenabled"];
-            $arrayReturn[$key]["dateembauchement"] = $user["dateembauchement"];
-            $arrayReturn[$key]["idTeam"] = $user["idTeam"];
-            $arrayReturn[$key]["teamName"] = $user["teamName"];
-            $arrayReturn[$key]["teamenabled"] = $user["teamenabled"];
-        }
-
-        return $arrayReturn;
+        return $teams;
     }
 }
